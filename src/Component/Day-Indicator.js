4d2fb94960156438790da8aabebff477aebd40ef
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-03-19 21:11:36 +0530

    updated

--- src/Component/Day-Indicator.js
+++ src/Component/Day-Indicator.js
@@ -5,7 +5,7 @@ const DayIndicator = () => {
     <div>
         <div className='flex gap-4 sm:flex-col items-center justify-center max-w-2xl'>
             <h3 className='text-center font-semibold text-md sm:text-lg'>Indicator</h3>
-            <ul className='' style={{width : "100%"}}>
+            <ul className='flex flex-col gap-2 md:flex-row' style={{width : "100%"}}>
                 <li className='flex gap-4 items-center justify-start '><span className='text-left'>Present Mark</span><span className='present '></span></li>
                 <li className='flex gap-4 items-center justify-start'><span className='text-left'>Absent Mark</span><span className='absent '></span></li>
                 <li className='flex gap-4 items-center justify-start'><span className='text-left'>Half-Day Mark</span><span className='half-day '></span></li>
