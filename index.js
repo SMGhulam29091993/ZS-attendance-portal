f68e4716c4ee1100febc24bcdb30217529fd7fa9
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-03-19 19:26:43 +0530

    updated the port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const db = require("./config/mongoose.js");
 const errHandlerMiddlware = require("./config/errorHanderMiddleware.js");
