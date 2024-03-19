4a8df672cd5b86c05007f266ae6fdf16c692642a
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-03-19 15:12:11 +0530

    added the error Handler

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const morgan = require('morgan');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const app = express();
-
 const PORT = 8000;
 
+const db = require("./config/mongoose.js");
+const errHandlerMiddlware = require("./config/errorHanderMiddleware.js");
 app.use(cors());
 
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+app.use(errHandlerMiddlware);
 
 
 
