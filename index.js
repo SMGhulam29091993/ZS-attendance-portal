c0757e52d909016034ccc77b404d794fc3f285e4
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-03-19 19:13:12 +0530

    commented morgan

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static("uploads"))
